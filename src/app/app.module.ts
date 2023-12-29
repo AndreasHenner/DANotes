c03b5454ca7a1c8df32511eff4425dacf1420482
Andreas Henner <134337273+AndreasHenner@users.noreply.github.com>
2023-12-29 17:13:10 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NoteComponent } from './note-list/note/note.component';
+import { FormsModule } from '@angular/forms';
+import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NoteListComponent,
+    NoteComponent,
+    AddNoteDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"danotes-7e507","appId":"1:194713272554:web:ab53d78cabb121d7185793","storageBucket":"danotes-7e507.appspot.com","apiKey":"AIzaSyBZa7LiPhy4JwkFEGCecYQGhJz-uD-e0zg","authDomain":"danotes-7e507.firebaseapp.com","messagingSenderId":"194713272554"})),
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp({"projectId":"danotes-7e507","appId":"1:194713272554:web:a8d977877683ffe3185793","storageBucket":"danotes-7e507.appspot.com","apiKey":"AIzaSyBZa7LiPhy4JwkFEGCecYQGhJz-uD-e0zg","authDomain":"danotes-7e507.firebaseapp.com","messagingSenderId":"194713272554"})),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/firebase-services/note-list.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface'
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteListService {
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {}
+   // const itemCollection = collection(this.firestore, 'items');
+   
+
+   getNotesRef() {
+    return collection(this.firestore, 'notes');
+   }
+
+   getTrahsRef() {
+    return collection(this.firestore, 'trash');
+   }
+
+   getSingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId);
+   }
+}
