5698a46647fe678db8d029c8f9daef1df519b17b
Andreas Henner <134337273+AndreasHenner@users.noreply.github.com>
2023-12-31 12:34:40 +0100

    onSnapShot

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,28 +1,75 @@
 import { Injectable, inject } from '@angular/core';
-import { Note } from '../interfaces/note.interface'
-import { Firestore, collection, doc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Note } from '../interfaces/note.interface';
+import {Firestore, collection, doc, collectionData, onSnapshot} from '@angular/fire/firestore';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  firestore: Firestore = inject(Firestore);
+  items$; // ist ein Observable-man kann auf Veränderungen damit reagieren
+  items;
 
-  constructor() {}
-   // const itemCollection = collection(this.firestore, 'items');
+  unsubList;
+  unsubSingle;
+
+  firestore: Firestore = inject(Firestore); // Firestore (Datenbank) wird in Angular eingebunden
+
+  constructor() {
+    //onSnapshot
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element.data());
+      });
+    });
+
+    this.unsubSingle = onSnapshot(this.getSingleDocRef("notes", "sadad3sfsdgdsgf"), (element) => {
+    });
+    
+    
+
+//collectionData
+    this.items$ = collectionData(this.getNotesRef());
+
+    this.items = this.items$.subscribe((list) => {
+      list.forEach((element) => {
+        console.log(element);
+      });
+    })
    
+  }
 
-   getNotesRef() {
+  setNoteObject(obj: any, id: string): Note {
+  return{
+  id: id,
+  type: obj.type || "note",
+  title: obj.title || "",
+  content: obj.content || "",
+  marked: obj.marked || false,
+}
+  }
+
+  ngonDestroy() {
+    this.items.unsubscribe(); // beendet subscribe
+    this.unsubList();
+    this.unsubSingle();
+  }
+
+  // hier wird auf notes in der Datenbank zugegriffen
+  getNotesRef() {
     return collection(this.firestore, 'notes');
-   }
+  }
 
-   getTrahsRef() {
+  // hier wird auf Trash in der Datenbank zugegriffen
+  getTrahsRef() {
     return collection(this.firestore, 'trash');
-   }
+  }
 
-   getSingleDocRef(colId:string, docId:string) {
+  // hier wird auf ein einzelnes Dokument in der Datenbank zugegriffen
+  getSingleDocRef(colId: string, docId: string) {
     return doc(collection(this.firestore, colId), docId);
-   }
+  }
 }
