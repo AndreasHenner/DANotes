d92fbfd629e22b5c43f5931b2d82a4941c012599
Andreas Henner <134337273+AndreasHenner@users.noreply.github.com>
2024-01-03 22:44:11 +0100

    deleteNote

--- src/app/add-note-dialog/add-note-dialog.component.ts
+++ src/app/add-note-dialog/add-note-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
 import { NoteListService } from '../firebase-services/note-list.service'
 
+
 @Component({
   selector: 'app-add-note-dialog',
   templateUrl: './add-note-dialog.component.html',
@@ -10,17 +11,25 @@ import { NoteListService } from '../firebase-services/note-list.service'
 export class AddNoteDialogComponent {
   @Output() addDialogClosed: EventEmitter<boolean> = new EventEmitter();
   title = "";
-  description = "";
+  content = "";
 
   constructor(private noteService: NoteListService){}
 
   closeDialog() {
     this.title = "";
-    this.description = "";
+    this.content = "";
     this.addDialogClosed.emit(false);
   }
 
   addNote(){
+    let note: Note = {    
+      type: "note",
+      title:this.title,
+      content:this.content,
+      marked: false,
+    }
+    this.noteService.addNote(note, "notes"); // Note wird zu notes hinzugefügt
+
     //beachte das closeDialog() zum Schluss kommt, denn es leert die Variablen
     this.closeDialog();
   }
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import {Firestore, collection, doc, collectionData, onSnapshot} from '@angular/fire/firestore';
-
+import {Firestore, collection, doc, onSnapshot, addDoc, updateDoc, deleteDoc} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +8,6 @@ import {Firestore, collection, doc, collectionData, onSnapshot} from '@angular/f
 export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
-
 
   unsubTrash;
   unsubNotes;
@@ -21,14 +19,63 @@ export class NoteListService {
     this.unsubTrash = this.subTrashList();
   }
 
+  // löschen von Notes -- cruD
+  async deleteNote(colId: "notes" | "trash", docId: string) {
+   await deleteDoc(this.getSingleDocRef(colId, docId)).catch(
+    (err) => { console.log(err)}
+   );
+  }
+
+
+  // Updaten(editieren) der Notes -- cr U d
+  async updateNote(note: Note) {
+    if (note.id) {
+      let docRef = this.getSingleDocRef(this.getColIdFromNote(note), note.id);
+      await updateDoc(docRef, this.getCleanJson(note)).catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+
+  getColIdFromNote(note: Note) {
+    if (note.type == 'note') {
+      return 'notes';
+    } else {
+      return 'trash';
+    }
+  }
+
+  getCleanJson(note: Note) {
+    return {
+      type: note.type,
+      title: note.title,
+      content: note.content,
+      marked: note.marked,
+    }
+  }
+
+  // hinzufügen einer Note -- Crud
+  async addNote(item: Note, colId: "notes" | "trash") {
+    if (colId == "notes") {
+      await addDoc(this.getNotesRef(), item);
+    } else {
+      await addDoc(this.getTrashRef(), item).catch((err) => {
+        console.error(err);
+    }).then((docRef) => {
+        console.log('Document written with ID: ', docRef?.id);
+      });
+    }
+    }
+  
+
   setNoteObject(obj: any, id: string): Note {
-  return{
-  id: id,
-  type: obj.type || "note",
-  title: obj.title || "",
-  content: obj.content || "",
-  marked: obj.marked || false,
-}
+    return {
+      id: id,
+      title: obj.title || '',
+      type: obj.type || 'note',
+      content: obj.content || '',
+      marked: obj.marked || false,
+    };
   }
 
   ngonDestroy() {
@@ -36,10 +83,12 @@ export class NoteListService {
     this.unsubNotes();
   }
 
+
+  // read Notes -- c R ud
   subTrashList() {
-   return onSnapshot(this.getTrashRef(), (list) => {
-    this.trashNotes = [];
-      list.forEach(element => {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach((element) => {
         this.trashNotes.push(this.setNoteObject(element.data(), element.id));
       });
     });
@@ -48,10 +97,8 @@ export class NoteListService {
   subNotesList() {
     return onSnapshot(this.getNotesRef(), (list) => {
       this.normalNotes = [];
-      list.forEach(element => {
+      list.forEach((element) => {
         this.normalNotes.push(this.setNoteObject(element.data(), element.id));
-        console.log(this.setNoteObject(element.data(), element.id));
-        console.log(element.data());
       });
     });
   }
